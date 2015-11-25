a10afd4108a56ba4247cb5de4146bc067504e260
Jana Nash-Siegle <jsiegle@jlsdesk.com>
2015-11-24 20:24:31 -0600

    datatypes

---
+++ Week1/datatypes.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head lang = "en">
+	<meta charset = "UTF-8">
+	<title>datatypes</title>
+</head>
+<body>
+
+	<?php
+
+	$var1 = 10;
+	echo $var1;
+
+	echo "<br />";
+
+	$var1 = 100;
+	echo $var1;
+
+	echo "<br />";
+
+	$var2 = "Hello world";
+	echo $var2;
+
+	?>
+
+
+
+
+
+</body>
+</html>
+
+
+
+
