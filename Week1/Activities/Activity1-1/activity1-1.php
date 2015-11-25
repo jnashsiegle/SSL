2c6585f765388fe0ce78a08427354dec1db50134
Jana Nash-Siegle <jsiegle@jlsdesk.com>
2015-11-25 01:46:19 -0600

    ...

--- Week1/Activities/Activity1-1/activity1-1.php
+++ Week1/Activities/Activity1-1/activity1-1.php
@@ -11,16 +11,10 @@
 	$name = "Jana Nash-Siegle";
 	$age = 52;
 	echo "1.  My name is " . $name . " and my age is " . $age . ".";
-	?><br /><br />
-<hr>
-	
-	
+	?><br /><br /><hr>	
 
-	
+	<?php echo "1.  " . $name . " " . $age; ?>  //Double Quotes<br /><br />	
 
-	<?php echo "1.  " . $name . " " . $age; ?>  //Double Quotes<br /><br />
-
-	
 	<?php echo "2.  " . $name. ' ' . $age; ?> // Single Quotes<br /><br />
 
 	<?php
@@ -29,22 +23,16 @@
 	?> //$personindex<br /> <br />
 
 	<?php
-	$person = array("name" => "Jana Nash-Siegle", "age" => "52");
-	
+	$person = array("name" => "Jana Nash-Siegle", "age" => "52");	
 	?> 
-	<?php echo "4.  " . $person["name"]. " " . $person["age"];?>  // key/value pairs<br /><br />
 
+	<?php echo "4.  " . $person["name"]. " " . $person["age"];?>  // key/value pairs<br /><br />
+	
 	<?php $age = null;
 	echo "5.  " . $age; ?> // NULL -the result is nothing echo's out, there is no age value any longer, null removed the original value of 52<br /><br />
-
-	 <?php unset($name);
-	 echo "6.  " . $name; ?>  // When you unset() it becomes an undefined variable  because the unset() destroyed the value
 	
-
-
-
-
-
-
+	<?php unset($name);
+	echo "6.  " . $name; ?>  // When you unset() it becomes an undefined variable  because the unset() destroyed the value
+	
 </body>
 </html>
