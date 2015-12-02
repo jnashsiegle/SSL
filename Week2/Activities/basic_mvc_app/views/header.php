6e1da0107af43c2bd5e12a717dd2a50bbe439937
Jana Nash-Siegle <jsiegle@jlsdesk.com>
2015-12-02 15:52:07 -0600

    Model View Controller

---
+++ Week2/Activities/basic_mvc_app/views/header.php
@@ -0,0 +1,20 @@
+<?php
+/*
+* 	======================================
+*	PROJECT:  	SSL - LAB 03 PREP
+*	FILE: 		views/header.php
+*	AUTHOR:		Jana Nash-Siegle
+*	CREATED: 	12/2/2015
+*	======================================
+*/
+?>
+<!DOCTYPE html>
+<html>
+<head lang = "en">
+	<meta charset = "UTF-8">
+	<title>Captcha</title>
+	<link rel = "stylesheet" href = "public/css/styles.css">
+</head>
+<body>
+
+<h2>Jana's Captcha Validation</h2>
